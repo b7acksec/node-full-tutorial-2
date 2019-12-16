8a84761d447a005d2c7c3a061d9ea5a6d3fa6955
Timerkhanov Timur <djtimone@inewsadmins-MacBook-Pro-3.local>
2019-12-16 17:48:28 +0600

    lesson 30

--- models/course.js
+++ models/course.js
@@ -17,15 +17,37 @@ class Course {
       img: this.img,
       id: this.id
     }
-  }
+  } 
 
+  static async update(course) {
+    const courses = await Course.getAll()
+
+    const idx = courses.findIndex(c => c.id === course.id)
+    courses[idx] = course
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        }
+      )
+    })
+  }
+  
   async save() {
     const courses = await Course.getAll()
     courses.push(this.toJSON())
     
     return new Promise((resolve, reject) => {
       fs.writeFile(
-        path.join('__dirname', '..', 'data', 'courses.json')
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
         (err) => {
           if (err) {
             reject(err)
@@ -41,17 +63,21 @@ class Course {
     return new Promise((resolve, reject) => {
       fs.readFile(
         path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses), 
         'utf-8',
         (err, content) => {
           if (err) {
-          reject(err)
+            reject(err)
           } else {
             resolve(JSON.parse(content)) 
           }
         }
       )
     })
+  }
+
+  static async getById(id) {
+    const courses = await Course.getAll()
+    return courses.find(c => c.id === id) 
   }
 }
 
