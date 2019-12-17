82ecb1047844439f216df093bbeb3353b4c3e00d
Timerkhanov Timur <djtimone@inewsadmins-MacBook-Pro-3.local>
2019-12-17 17:09:25 +0600

    lesson 35

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 
@@ -15,8 +16,9 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+app.use('/card', cardRoutes)
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public'))) 
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
